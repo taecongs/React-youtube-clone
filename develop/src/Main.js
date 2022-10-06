93259fea1d5147f75159f3867682ce830e965d64
taecongs <bks0927w@gmail.com>
2022-10-06 22:04:24 +0900

    Develop : 2022-10-06

--- develop/src/Main.js
+++ develop/src/Main.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import Header from './Header';
+import Content from './Content';
+// import Aside from './Aside';
 
 const Main = () => {
     return(
         <div>
             <Header />
+            {/* <Aside /> */}
+            <Content />
         </div>
     );
 }
