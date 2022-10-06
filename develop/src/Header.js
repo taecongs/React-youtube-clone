13a8ef191c75f1cc9625f04153301d438fc16f1e
taecongs <bks0927w@gmail.com>
2022-10-06 15:34:41 +0900

    Develop : 2022-10-06

---
+++ develop/src/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+const Header = () => {
+    return(
+        <div>
+            <div>
+                <h1>헤더</h1>
+            </div>
+        </div>
+    );
+}
+
+export default Header;
