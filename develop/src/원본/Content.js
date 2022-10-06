cdf63d0bbbc51525ed6c846c6187424cd01e1ed2
taecongs <bks0927w@gmail.com>
2022-10-07 00:06:06 +0900

    Develop : 2022-10-06

--- develop/src/원본/Content.js
+++ develop/src/원본/Content.js
@@ -3,8 +3,11 @@ import React from 'react';
 import './css/content.scss';
 
 const Content = () => {
-    return (
-        <main>
+    return(
+        <main className='content'>
+            <div>
+
+            </div>
         </main>
     );
 }
